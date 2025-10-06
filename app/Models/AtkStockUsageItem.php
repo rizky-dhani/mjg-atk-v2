40dce5f7e2c6c4bcaff39c897dd6c3132388bf83
Rizky Dhani <rizkydhni03@gmail.com>
2025-10-06 14:58:28 +0700

    Add AtkStockUsageItem model with migration

---
+++ app/Models/AtkStockUsageItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AtkStockUsageItem extends Model
+{
+    protected $fillable = [
+        'usage_id',
+        'item_id',
+        'quantity_used'
+    ];
+
+    public function usage()
+    {
+        return $this->belongsTo(AtkStockUsage::class, 'usage_id');
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(AtkItem::class, 'item_id');
+    }
+}
