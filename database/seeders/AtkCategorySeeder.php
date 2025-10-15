dddb57a8784b46704217c0cbeda1aa4f0eb716fd
Rizky Dhani <rizkydhani15@gmail.com>
2025-10-15 13:21:49 +0700

    Add seeders for ATK inventory management and user roles
    - Created ApprovalFlowSeeder to seed approval flows for ATK stock requests.
    - Added AtkCategorySeeder to seed various ATK categories.
    - Implemented AtkDivisionInventorySettingSeeder to set inventory limits for divisions.
    - Developed AtkDivisionStockSeeder to manage stock levels for each division.
    - Introduced AtkItemSeeder to populate the ATK items database.
    - Updated DatabaseSeeder to include new seeders for roles, permissions, and user divisions.
    - Created PermissionSeeder to generate model permissions and assign them to the Super Admin role.
    - Removed RolePermissionSeeder and replaced it with RoleSeeder for better role management.
    - Added UserDivisionSeeder to seed user divisions.
    - Implemented UserSeeder to create default users with roles and permissions.

---
+++ database/seeders/AtkCategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AtkCategory;
+use App\Models\OfficeStationeryCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AtkCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AtkCategory::insert([
+            ['name' => 'Kertas'],
+            ['name' => 'Odner'],
+            ['name' => 'Buku dan Kwitansi'],
+            ['name' => 'Pulpen, Pensil, Stabilo'],
+            ['name' => 'Binder Clip'],
+            ['name' => 'Label T&J'],
+            ['name' => 'Lain-Lain'],
+            ['name' => 'Kop Surat'],
+            ['name' => 'Tambahan'],
+        ]);
+    }
+}
---
+++ database/seeders/UserDivisionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserDivision;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserDivisionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        UserDivision::insert([
+            ['name' => 'Accounting', 'initial' => 'ACC'],
+            ['name' => 'Business Development', 'initial' => 'BDV'],
+            ['name' => 'Finance', 'initial' => 'FIN'],
+            ['name' => 'Human Capital', 'initial' => 'HC'],
+            ['name' => 'General Affairs', 'initial' => 'GA'],
+            ['name' => 'Human Capital General', 'initial' => 'HCG'],
+            ['name' => 'Import and Purchasing', 'initial' => 'IPC'],
+            ['name' => 'Information Technology', 'initial' => 'ITD'],
+            ['name' => 'Legal and Compliance', 'initial' => 'LAC'],
+            ['name' => 'Medical Affairs', 'initial' => 'MED'],
+            ['name' => 'Marketing Blood Bank', 'initial' => 'MBB'],
+            ['name' => 'Marketing Hospital', 'initial' => 'MHO'],
+            ['name' => 'Marketing Primary Care', 'initial' => 'MPC'],
+            ['name' => 'Marketing Primary Health', 'initial' => 'MPH'],
+            ['name' => 'Marketing Support', 'initial' => 'MKS'],
+            ['name' => 'Regulatory Affairs', 'initial' => 'RA'],
+            ['name' => 'Quality Assurance', 'initial' => 'QA'],
+            ['name' => 'Revenue Funnel', 'initial' => 'REV'],
+        ]);
+    }
+}
