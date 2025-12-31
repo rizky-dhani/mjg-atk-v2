6be88a82f46df6b9fa8676cbcc5dd54131f0e728
Rizky Dhani <rizkydhni03@gmail.com>
2025-12-31 16:01:09 +0700

    feat(filament): add UserDivision form and table schemas with name attribute

---
+++ app/Filament/Resources/UserDivisions/Schemas/UserDivisionForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\UserDivisions\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class UserDivisionForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('initial')
+                    ->required(),
+                Textarea::make('description')
+                    ->default(null)
+                    ->columnSpanFull(),
+            ]);
+    }
+}
