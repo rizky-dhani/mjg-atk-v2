80cb5fbe771a933d753ceb98b512611b53087063
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-14 15:41:30 +0700

    fix(widget): use Filament button component for QuickCreate styling

--- resources/views/filament/widgets/quick-create.blade.php
+++ resources/views/filament/widgets/quick-create.blade.php
@@ -2,21 +2,19 @@
     <x-filament::section heading="Quick Create">
         <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
             @foreach($this->getActions() as $action)
-                <a
-                    href="{{ $action['url'] }}"
-                    class="flex flex-col items-center justify-center gap-2 p-4 rounded-xl border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 hover:bg-gray-50 dark:hover:bg-gray-800 transition cursor-pointer"
+                <x-filament::button
+                    tag="a"
+                    :href="$action['url']"
+                    :icon="$action['icon']"
+                    color="{{ $action['color'] }}"
+                    size="lg"
+                    class="justify-center"
                 >
-                    <x-filament::icon
-                        :icon="$action['icon']"
-                        :class="'w-8 h-8 text-' . $action['color'] . '-500'"
-                    />
-                    <span class="text-sm font-semibold text-gray-700 dark:text-gray-200">
-                        {{ $action['label'] }}
-                    </span>
-                    <span class="text-xs text-gray-500 dark:text-gray-400 text-center">
-                        {{ $action['description'] }}
-                    </span>
-                </a>
+                    <div class="flex flex-col items-center gap-1">
+                        <span>{{ $action['label'] }}</span>
+                        <span class="text-xs opacity-70">{{ $action['description'] }}</span>
+                    </div>
+                </x-filament::button>
             @endforeach
         </div>
     </x-filament::section>
