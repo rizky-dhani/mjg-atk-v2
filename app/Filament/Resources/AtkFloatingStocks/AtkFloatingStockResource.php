d07798fe8e9c2b03b1bf072257fd1a71ca35915d
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-14 15:11:27 +0700

    chore(ui): change Stok Umum icon from OutlinedRectangleStack to ArchiveBox

--- app/Filament/Resources/AtkFloatingStocks/AtkFloatingStockResource.php
+++ app/Filament/Resources/AtkFloatingStocks/AtkFloatingStockResource.php
@@ -19,7 +19,7 @@ class AtkFloatingStockResource extends Resource
 {
     protected static ?string $model = AtkFloatingStock::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ArchiveBox;
 
     public static function getNavigationGroup(): ?string
     {
