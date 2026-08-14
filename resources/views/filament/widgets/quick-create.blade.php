c8f816b67a4da986f9c34246782307b97eda869f
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-14 15:57:41 +0700

    fix(widget): revert QuickCreate to page links — widgets lack action modals

--- resources/views/filament/widgets/quick-create.blade.php
+++ resources/views/filament/widgets/quick-create.blade.php
@@ -2,7 +2,14 @@
     <x-filament::section heading="Quick Create">
         <div class="flex flex-wrap justify-center items-center px-4 py-2">
             @foreach($this->getActions() as $action)
-                {{ $action }}
+                <x-filament::button
+                    tag="a"
+                    :href="$action['url']"
+                    :icon="$action['icon']"
+                    color="{{ $action['color'] }}"
+                >
+                    {{ $action['label'] }}
+                </x-filament::button>
             @endforeach
         </div>
     </x-filament::section>
