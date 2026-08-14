75ecd1e3bf6b75e3764978718226aeba64d891c8
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-14 15:53:22 +0700

    fix(widget): force button spacing with explicit margin

--- resources/views/filament/widgets/quick-create.blade.php
+++ resources/views/filament/widgets/quick-create.blade.php
@@ -1,6 +1,6 @@
 <x-filament-widgets::widget>
     <x-filament::section heading="Quick Create">
-        <div class="flex flex-wrap justify-center gap-4 px-4 py-2">
+        <div class="flex flex-wrap justify-center items-center px-4 py-2 [&>*:not(:last-child)]:mr-6">
             @foreach($this->getActions() as $action)
                 <x-filament::button
                     tag="a"
