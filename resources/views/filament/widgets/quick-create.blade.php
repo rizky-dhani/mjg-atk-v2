fb5d11103d3b509608190cea854b3be06421b0f5
Rizky Dhani <rizkydhni03@gmail.com>
2026-08-19 09:26:17 +0700

    style: make quick create buttons full width

--- resources/views/filament/widgets/quick-create.blade.php
+++ resources/views/filament/widgets/quick-create.blade.php
@@ -1,12 +1,13 @@
 <x-filament-widgets::widget>
     <x-filament::section heading="Quick Create">
-        <div class="flex flex-wrap justify-center items-center px-4 py-2">
+        <div class="flex flex-col gap-2 px-4 py-2">
             @foreach($this->getActions() as $action)
                 <x-filament::button
                     tag="a"
                     :href="$action['url']"
                     :icon="$action['icon']"
                     color="{{ $action['color'] }}"
+                    class="w-full"
                 >
                     {{ $action['label'] }}
                 </x-filament::button>
